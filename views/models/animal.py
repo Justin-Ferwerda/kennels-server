b1ec67df5b749a33e5669e2a8e689bce986f7377
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-10-20 19:00:33 -0500

    connected SQLite3 db

---
+++ views/models/animal.py
@@ -0,0 +1,9 @@
+class Animal():
+    """animal class"""
+    def __init__(self, id, name, species, status, location_id, customer_id):
+        self.id = id
+        self.name = name
+        self.breed = species
+        self.status = status
+        self.location_id = location_id
+        self.customer_id = customer_id
